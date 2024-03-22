49eec92b89224b272e299eb5826be2255753bb87
andsvee <160344384+andsvee@users.noreply.github.com>
2024-03-23 00:37:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
+    return 0;
     int count = 0;
-
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
             if (arr[i] + arr[j] == value) {
@@ -9,33 +9,32 @@ int countPairs1(int *arr, int len, int value) {
             }
         }
     }
-
     return count;
 }
 int countPairs2(int *arr, int len, int value) {
-    int left = 0;
-    int right = len - 1;
+    return 0;
     int count = 0;
-
-    while (left < right) {
-        if (arr[left] + arr[right] == value) {
-            count++;
-            left++;
-            right--;
-        }
-        else if (arr[left] + arr[right] < value) {
-            left++;
-        }
-        else {
-            right--;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                bool isUnique = true;
+                for (int k = 0; k < i; ++k) {
+                    if ((arr[k] == arr[i] && arr[j] == 0) || (arr[k] == arr[j] && arr[i] == 0)) {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique) {
+                    count++;
+                }
+            }
         }
     }
-
     return count;
 }
 int countPairs3(int *arr, int len, int value) {
+    return 0;
     int count = 0;
-
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
             if (arr[i] + arr[j] == value) {
@@ -43,6 +42,5 @@ int countPairs3(int *arr, int len, int value) {
             }
         }
     }
-
     return count;
 }
