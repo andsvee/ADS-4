9192117d2b12b7cc7ddb11e72a1a83f4a1df5554
andsvee <160344384+andsvee@users.noreply.github.com>
2024-03-23 00:41:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,43 +1,61 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            }
-        }
+  int k = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        k++;
+      }
     }
-    return count;
+  }
+  return k;
 }
 int countPairs2(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
-            if (arr[i] + arr[j] == value) {
-                bool isUnique = true;
-                for (int k = 0; k < i; ++k) {
-                    if ((arr[k] == arr[i] && arr[j] == 0) || (arr[k] == arr[j] && arr[i] == 0)) {
-                        isUnique = false;
-                        break;
-                    }
-                }
-                if (isUnique) {
-                    count++;
-                }
-            }
+  int r = len - 1;
+  while (arr[r] > value) {
+    r--;
+  }
+  int k = 0;
+  for (int i = 0; i < r; i++) {
+    for (int j = r; j > i; j--) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        k++;
+      } else if (arr[i] + arr[j] < value) {
+        break;
         }
     }
-    return count;
+  }
+  return k;
 }
 int countPairs3(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            }
+  int k = 0;
+  int left;
+  int right, mid, sredn;
+  for (int i = 0; i < len; ++i) {
+    right = len - 1;
+    left = i + 1;
+    sredn = value - arr[i];
+    while (left <= right) {
+      mid = (left + right) / 2;
+      if (arr[mid] == sredn) {
+        k++;
+        int p = mid - 1;
+        while ((p > i) && (arr[p] == arr[mid])) {
+          k++;
+          p--;
+        }
+        int p2 = mid + 1;
+        while ((i < p2) && (arr[p2] == arr[mid])) {
+          k++;
+          p2++;
+        }
+        break;
+        } else if (arr[mid] > sredn) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
         }
     }
-    return count;
+  }
+  return k;
 }
